172601fee93b2a93efe932dfde59c6266905d6b6
wongkhamdee <jetsada081@gmail.com>
2022-10-26 16:13:51 +0700

    เพิ่ม text ในหน้า wallet

--- Code/numfu/lib/screen/wallet.dart
+++ Code/numfu/lib/screen/wallet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:numfu/screen/index.dart';
+import 'package:numfu/utility/my_constant.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Wallet extends StatefulWidget {
   static const routeName = '/';
@@ -16,15 +19,45 @@ class _WalletState extends State<Wallet> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wallet'),
+        title: Text(
+          'Payment',
+          style: GoogleFonts.khand(textStyle: TextStyle(fontSize: 36)),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
       body: Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Wallet Screen'),
-        ],
+        children: <Widget>[buildMywallet(), Text('ระบุจำนวนเงิน (THB)')],
       )),
+    );
+  }
+}
+
+class buildMywallet extends StatelessWidget {
+  const buildMywallet({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 110,
+      color: MyCostant.primary,
+      child: Row(
+        children: <Widget>[
+          /*Icon(
+            Icons.location_history,
+            color: MyCostant.light,
+          ),*/
+          Text(
+            'ยอดเงินคงเหลือ',
+            style: TextStyle(
+                fontSize: 35, color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+        ],
+      ),
     );
   }
 }
