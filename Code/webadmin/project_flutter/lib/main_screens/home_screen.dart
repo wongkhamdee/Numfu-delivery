bdc66f01a7947620557a41b724f05c4800155162
Thiti_Bamrungna <88425719+ThitiBamrung@users.noreply.github.com>
2022-11-13 23:37:35 +0700

    เพื่มหน้าฟอร์มของเเต่ละหน้า
    Dashboard
    Commission
    Promotion
    User

--- Code/webadmin/project_flutter/lib/main_screens/home_screen.dart
+++ Code/webadmin/project_flutter/lib/main_screens/home_screen.dart
@@ -1,18 +1,140 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: const HomeScreen());
+  }
+}
+
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Widget> _screens = [
+    // Content for Home tab
+    Container(
+      color: Colors.yellow.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Dashboard',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Feed tab
+    Container(
+      color: Colors.purple.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Commissions',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Favorites tab
+    Container(
+      color: Colors.red.shade100,
+      alignment: Alignment.center,
+      child: const Text(
+        'Promotion',
+        style: TextStyle(fontSize: 40),
+      ),
+    ),
+    // Content for Settings tab
+    Container(
+      color: Colors.pink.shade300,
+      alignment: Alignment.center,
+      child: const Text(
+        'Users',
+        style: TextStyle(fontSize: 40),
+      ),
+    )
+  ];
+
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome to admin web"),
+        title: const Text("Hello Administator"),
+      ),
+      bottomNavigationBar: MediaQuery.of(context).size.width < 640
+          ? BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              unselectedItemColor: Colors.grey,
+              selectedItemColor: Color(0xFFFF8126),
+              // called when one tab is selected
+              onTap: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              // bottom tab items
+              items: const [
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.home), label: 'Dashboard'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.feed), label: 'Commissions'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.favorite), label: 'Promotions'),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.settings), label: 'Users')
+                ])
+          : null,
+      body: Row(
+        children: [
+          if (MediaQuery.of(context).size.width >= 640)
+            NavigationRail(
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              selectedIndex: _selectedIndex,
+              destinations: const [
+                NavigationRailDestination(
+                    icon: Icon(Icons.home), label: Text('Dashboard')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.feed), label: Text('Commissions')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.favorite), label: Text('Promotions')),
+                NavigationRailDestination(
+                    icon: Icon(Icons.settings), label: Text('Users')),
+              ],
+
+              labelType: NavigationRailLabelType.all,
+              selectedLabelTextStyle: const TextStyle(
+                color: Colors.teal,
+              ),
+
+              unselectedLabelTextStyle: const TextStyle(),
+              // Called when one tab is selected
+              leading: Column(
+                children: const [
+                  SizedBox(
+                    height: 8,
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    child: Icon(Icons.person),
+                  ),
+                ],
+              ),
+            ),
+          Expanded(child: _screens[_selectedIndex])
+        ],
       ),
     );
   }
