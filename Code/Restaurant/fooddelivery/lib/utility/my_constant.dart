ee950171ae1cf074149c669e716fb3e609a06de8
Thiti_Bamrungna <88425719+ThitiBamrung@users.noreply.github.com>
2023-02-24 10:34:15 +0700

    Font-end ร้านค้า
    หน้า
    1.เข้าสู่ระบบ
    2.หน้ารายละเอียดส่วนที่1

---
+++ Code/Restaurant/fooddelivery/lib/utility/my_constant.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  //General
+  static String appName = 'Food Delivery';
+  //Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = '/buyerService';
+  static String routeSalerService = '/salerService';
+  static String routeRiderService = '/riderService';
+
+  //Image
+  static String logo = 'images/image1.png';
+
+  //color
+  static Color primary = Color(0xffFF8126);
+  static Color dark = Color(0xffC55200);
+  static Color light = Color(0xffFFC077);
+
+  //style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
+  TextStyle h4Style() => TextStyle(
+        fontSize: 20,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h5Style() => TextStyle(
+        fontSize: 18,
+        color: Colors.black,
+        fontWeight: FontWeight.normal,
+      );
+
+  ButtonStyle myButtonStyle() => ElevatedButton.styleFrom(
+        primary: MyConstant.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      );
+}
